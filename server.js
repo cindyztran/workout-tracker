580c8f903cf8b55548da3fd9abfb8c0a8a0e5da0
cindytran <cindyztran@gmail.com>
2021-06-14 13:48:50 -0700

    wip: initializes port and adds homepage

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+//initialize and configure env
 require('dotenv').config();
 
 //Dependencies 
 const express = require('express');
 
+//Port: allow use of Heroku's port or local port, depending on the environment
+const PORT = process.env.PORT || 3000;
+
 //Initialize Express
 const app = express();
 
+//Home Route
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
 //Tell Express to listen
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Express is listening on port ${PORT}`);
 });
