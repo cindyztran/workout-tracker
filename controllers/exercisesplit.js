58be344d32a0f28a2d25a28630adf20c56e30642
cindytran <cindyztran@gmail.com>
2021-06-14 14:42:45 -0700

    wip: adds exercise split new page

--- controllers/exercisesplit.js
+++ controllers/exercisesplit.js
@@ -4,8 +4,15 @@ const router = require('express').Router();
 //Define routes/controllers
 
 //Mount controller on /exercisesplit
+
+//Index
 router.get('/', (req, res) => {
     res.render('exercisesplit/index');
+});
+
+//New
+router.get('/new', (req, res) => {
+    res.render('exercisesplit/new');
 });
 
 //Exports
