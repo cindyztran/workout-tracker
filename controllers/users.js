9a85f949c436158aaa9945f79c6887749c1813a0
cindytran <cindyztran@gmail.com>
2021-06-15 11:09:52 -0700

    feat: adds registration page

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,14 @@ const userRouter = require('express').Router();
 const User = require('../models/user.js');
 
 //New (Registration Page)
-
-
+userRouter.get('/new', (req, res) => {
+    res.render('users/new.ejs');
+});
 
 //Create (Registration Route)
 userRouter.post('/', (req, res) => {
     //overwrite user password with hashed password and pass into database
-    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(12));
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 
     User.create(req.body, (error, createdUser) => {
         res.redirect('/');
