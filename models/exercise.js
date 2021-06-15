d1dc845060b22d1835fca4baafd1c9286366c247
cindytran <cindyztran@gmail.com>
2021-06-14 17:39:12 -0700

    adds exercise model

---
+++ models/exercise.js
@@ -0,0 +1,19 @@
+//Dependencies
+const mongoose = require('mongoose');
+const Schema = mongoose
+
+
+//Set up Schema
+const exerciseSchema = new Schema ({
+    name: String,
+    sets: Number,
+    reps: Number,
+    weight: Number,
+
+}, { timestamps: true});
+
+//Compile Schema into a model
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+//Export model
+module.exports = Exercise; 
